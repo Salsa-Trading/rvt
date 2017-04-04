29f0f655bdb7373cec26500758749f368d4c55ac
Josh Heyse <jheyse@drwholdings.com>
2017-04-04 12:18:30 -0500

    code formating and linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 export {
   FieldDefinition as Field
 } from './List/Field';
+
 export {
   FieldSetDefinition as FieldSet
 } from './List/FieldSet';
+
 export {
   default as List,
   ListState,
   ListStateChangeType,
   isDataChange,
 } from './List';
+
 export {
   default as VirtualGrid,
   RowData
 } from './Grid/VirtualGrid';
+
 export {
   default as VirtualTable
 } from './VirtualTable';
