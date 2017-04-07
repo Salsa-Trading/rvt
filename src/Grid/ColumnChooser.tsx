8bcc947165b245f445e5c577f6585d0cfc2b9bb6
Josh Heyse <jheyse@drwholdings.com>
2017-04-07 11:06:46 -0500

    disable hide columns for showAlways, of a column that is currently filtered or sorted

--- src/Grid/ColumnChooser.tsx
+++ src/Grid/ColumnChooser.tsx
@@ -42,6 +42,7 @@ export default class ColumnChooser extends React.Component<ColumnChooserProps, v
                 type='checkbox'
                 id={field.name}
                 checked={!field.hidden}
+                disabled={field.showAlways || field.sortDirection || field.filter}
                 onChange={this.onChange.bind(this, field)}
               />
               {name}
