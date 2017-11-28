f70042442cfcac2ab3051df61e9ee95ebf87d093
Josh Heyse <jheyse@drwholdings.com>
2017-11-28 14:20:23 -0600

    added form to show/change virtualSize and offset

--- docs/examples/VirtualScroller.tsx
+++ docs/examples/VirtualScroller.tsx
@@ -4,23 +4,34 @@ import {autobind} from 'core-decorators';
 
 export default class VirtualScroller extends React.Component<{}, {
   scrollOffset: number;
+  virtualSize: number;
 }> {
 
   constructor(props, context) {
     super(props, context);
     this.state = {
-      scrollOffset: 0
+      scrollOffset: 0,
+      virtualSize: 1000
     };
   }
 
   @autobind
   public onScroll(scrollOffset: number) {
     this.setState({scrollOffset});
-    console.log(scrollOffset);
+  }
+
+  @autobind
+  public setVirtualSize(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({virtualSize: parseFloat(e.currentTarget.value)});
+  }
+
+  @autobind
+  public setScrollOffset(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({scrollOffset: parseFloat(e.currentTarget.value)});
   }
 
   public render() {
-    const {scrollOffset} = this.state;
+    const {scrollOffset, virtualSize} = this.state;
     const divStyle = {
       height: '100px',
       width: '100px'
@@ -28,14 +39,29 @@ export default class VirtualScroller extends React.Component<{}, {
     return (
       <div className='container-fluid'>
         <div className='row'>
-          <div className='col-sm-1'>
+          <div className='col-md-1'>
             <div style={divStyle}>
-              <Scroller margin={0} size={100} virtualSize={1000} visible={true} onScroll={this.onScroll} scrollOffset={scrollOffset} />
+              <Scroller margin={0} size={100} virtualSize={virtualSize} visible={true} onScroll={this.onScroll} scrollOffset={scrollOffset} />
             </div>
           </div>
-          <div className='col-sm-1'>
+          <div className='col-md-1' />
+          <div className='col-md-1'>
             <div style={divStyle}>
-              <Scroller orientation='horizontal' margin={0} size={100} virtualSize={1000} visible={true} onScroll={this.onScroll} scrollOffset={scrollOffset} />
+              <Scroller orientation='horizontal' margin={0} size={100} virtualSize={virtualSize} visible={true} onScroll={this.onScroll} scrollOffset={scrollOffset} />
+            </div>
+          </div>
+          <div className='col-md-4'>
+            <div className='form-group row'>
+              <label htmlFor='virtualSize' className='col-sm-2 col-form-label'>Virtual Size</label>
+              <div className='col-sm-4'>
+                <input type='number' className='form-control' id='virtualSize' value={virtualSize} onChange={this.setVirtualSize} />
+              </div>
+            </div>
+            <div className='form-group row'>
+              <label htmlFor='scrollOffset' className='col-sm-2 col-form-label'>Scroll Offset</label>
+              <div className='col-sm-4'>
+                <input type='number' className='form-control' id='scrollOffset' value={scrollOffset} onChange={this.setScrollOffset} />
+              </div>
             </div>
           </div>
         </div>
