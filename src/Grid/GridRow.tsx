d069270f8bfd8c9af56d1a349d7786c96a453bf4
Kurt Preston <kpreston@drw.com>
2018-02-27 11:34:37 -0600

    Only render GridRow when props change

--- src/Grid/GridRow.tsx
+++ src/Grid/GridRow.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { Field } from '../List/Field';
 import * as get from 'lodash.get';
+import { isEqual } from 'lodash';
 import isNil from '../utils/isNil';
 import { TableRowProps } from '../VirtualTable';
 import {GridRowProps, GridRowComponentProps, VirtualGridMouseEventHandler} from './types';
+import { autobind } from 'core-decorators';
 
 export function renderGridCell<TData>(field: Field, data: TData) {
   if(field.cell) {
@@ -24,7 +26,7 @@ export function renderGridCell<TData>(field: Field, data: TData) {
   }
 }
 
-export default class GridRow<TData> extends React.PureComponent<GridRowComponentProps<TData>, {}> {
+export default class GridRow<TData> extends React.Component<GridRowComponentProps<TData>, {}> {
 
   public static propTypes = {
     fields: PropTypes.any,
@@ -36,10 +38,39 @@ export default class GridRow<TData> extends React.PureComponent<GridRowComponent
     super(props, context);
   }
 
+  public shouldComponentUpdate(nextProps, nextState) {
+    return !isEqual(this.props, nextProps);
+  }
+
+  @autobind
   private onMouseEvent(eventHandler: VirtualGridMouseEventHandler, e: React.MouseEvent<any>) {
     const { data } = this.props;
     const td = (e.target as any).closest('td');
     eventHandler(e, data, td.dataset['field']);
+  }
+
+  @autobind
+  private fieldForMouseEvent(e: React.MouseEvent<any>): string {
+    const td = (e.target as any).closest('td');
+    return td.dataset['field'];
+  }
+
+  @autobind
+  private onClick(e: React.MouseEvent<any>) {
+    const { onClick, data } = this.props;
+    onClick(e, data, this.fieldForMouseEvent(e));
+  }
+
+  @autobind
+  private onDoubleClick(e: React.MouseEvent<any>) {
+    const { onDoubleClick, data } = this.props;
+    onDoubleClick(e, data, this.fieldForMouseEvent(e));
+  }
+
+  @autobind
+  private onMouseDown(e: React.MouseEvent<any>) {
+    const { onMouseDown, data } = this.props;
+    onMouseDown(e, data, this.fieldForMouseEvent(e));
   }
 
   public render() {
@@ -47,9 +78,9 @@ export default class GridRow<TData> extends React.PureComponent<GridRowComponent
 
     return (
       <tr
-        onClick={onClick && this.onMouseEvent.bind(this, onClick)}
-        onDoubleClick={onDoubleClick && this.onMouseEvent.bind(this, onDoubleClick)}
-        onMouseDown={onMouseDown && this.onMouseEvent.bind(this, onMouseDown)}
+        onClick={onClick && this.onClick}
+        onDoubleClick={onDoubleClick && this.onDoubleClick}
+        onMouseDown={onMouseDown && this.onMouseDown}
         {...rowProps}
       >
         {(fields || []).map(field => {
