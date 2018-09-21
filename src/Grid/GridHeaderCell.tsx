5a55950a94c67f6d8ad6a8387ab63fd1240a578d
Kurt Preston <kpreston@drw.com>
2018-09-21 09:49:48 -0500

    Show point cursor when header is clickable

--- src/Grid/GridHeaderCell.tsx
+++ src/Grid/GridHeaderCell.tsx
@@ -247,7 +247,7 @@ export default class GridHeaderCell extends React.Component<GridHeaderCellProps,
       <th key={name} className='grid-header' style={style} rowSpan={rowSpan} colSpan={colSpan} {...dataSet} ref={this.setRef}>
         <div className={`${headerClassName}`}>
           <div className='header' onMouseDown={this.onMouseDown} onClick={this.onClick}>
-            <div className='header-text' style={{cursor: 'default'}}>
+            <div className='header-text'>
               {this.renderHeader()}
             </div>
           </div>
