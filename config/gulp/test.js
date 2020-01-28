8009ed42c06af5d5cee7e510e28b4c3cb0062358
Kurt Preston <kpreston@drw.com>
2020-01-28 15:34:50 -0600

    Fixing karma tests

--- config/gulp/test.js
+++ config/gulp/test.js
@@ -1,6 +1,4 @@
-const gulp = require('gulp');
 const KarmaServer = require('karma').Server;
-const Xvfb = require('xvfb');
 const os = require('os');
 const karmaOptions = {
   configFile: __dirname + '/../karma.conf.js'
@@ -8,27 +6,21 @@ const karmaOptions = {
 
 // Execute client karma tests in a watch loop
 function karma(done) {
-  const xvfb = new Xvfb({silent: true});
   const karmaServer = new KarmaServer(karmaOptions, done);
   const debugUrl = `http://${os.hostname()}:${karmaServer.get('config').port}/debug.html`;
   console.log('Starting headless Chrome. To debug, open \x1b[94m' + debugUrl + '\x1b[0m');
-  xvfb.start(function() {
-    karmaServer.start();
-  });
+  karmaServer.start();
 }
 
 // Execute client karma tests once
 function karmaRun(done) {
-  const xvfb = new Xvfb({silent: true});
-  xvfb.start(function() {
-    const karmaServer = new KarmaServer(Object.assign(karmaOptions, {singleRun: true}));
+  const karmaServer = new KarmaServer(Object.assign(karmaOptions, {singleRun: true}));
 
-    karmaServer.on('run_complete', function (browsers, results) {
-      done(results.error ? 'There are test failures' : null);
-    });
-
-    karmaServer.start();
+  karmaServer.on('run_complete', function (browsers, results) {
+    done(results.error ? 'There are test failures' : null);
   });
+
+  karmaServer.start();
 }
 
 exports.karma = karma;
